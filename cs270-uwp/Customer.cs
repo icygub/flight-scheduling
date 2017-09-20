346ae1581404b3ab15a8e6581df9d1e981368406
icygub <jchubbs95@gmail.com>
2017-09-20 13:55:15 -0600

    added various classes

---
+++ cs270-uwp/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs270_uwp
+{
+    class Customer
+    {
+        private string name;
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            set; get;
+        }
+    }
+}
---
+++ cs270-uwp/CustomerBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs270_uwp
+{
+    class CustomerBase
+    {
+        List<Customer> customers = new List<Customer>();
+        public void addCustomer(String customer)
+        {
+            customers.Add(new Customer("John"));
+        }
+
+        public bool findCustomer(String customer)
+        {
+            foreach (Customer c in customers)
+            {
+                if (c.Name.Equals(customer))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void deleteCustomer(String customer) { }
+
+        public void editCustomer(String customer, String editedCustomer) { }
+    }
+}
+
